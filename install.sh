b9a28b17d8f557c54ad598c5a86323e45053aa90
FxMods <80445478+Fxmods@users.noreply.github.com>
2021-06-12 23:16:18 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -19,79 +19,79 @@ WHITE='\033[1;37m'
  echo -e " ${YELLOW}Iniciando..."
 sleep 4
 
-echo -e "Bot criado por:"
+echo -e "Bot Creado Por:"
 sleep 1
 
-echo -e " ${RED} ▀█▀ ▀▀█▀▀ █▀▀█ █░░░ █░▒█"
-echo -e " ${RED} ░█░ ░▒█░░ █▄▄█ █░░░ █░▒█"
-echo -e " ${RED} ▄█▄ ░▒█░░ █░▒█ █▄▄█ ▀▄▄▀"
+echo -e " ${RED} Felixcrack 409"
+echo -e " ${RED} ░Colaboradores░ Satan, CryBaby, Mr.Pato, Gracias a todos.▒"
+echo -e " ${RED} Suscríbete s YouTube: Felixcrack 409"
 sleep 2
 
-echo -e " ${YELLOW} Olá"
-echo -e " ${YELLOW} Todas as dependências necessárias serão instaladas"
-echo -e " ${YELLOW} só espera um pouquinho..."
+echo -e " ${YELLOW} Hola"
+echo -e " ${YELLOW} Se instalarán todas las dependencias necesarias."
+echo -e " ${YELLOW} Solo espera un poco..."
 sleep 5
 
-echo -e " ${CYAN}A instalar todas as dependências..."
+echo -e " ${CYAN}A Instalar Todas Las Dependencias ..."
 sleep 5
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get update"
+echo -e " ${PURPLE} Usando El Comando: apt-get update"
 echo -e " ${GREEN}"
 sleep 5
 apt-get update -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get upgrade"
+echo -e " ${PURPLE} Usando El Comando: apt-get upgrade"
 echo -e " ${GREEN}"
 sleep 5
 apt-get upgrade -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get install nodejs"
+echo -e " ${PURPLE} Usando El Comando: apt-get install nodejs"
 echo -e " ${GREEN}"
 sleep 5
 apt-get install nodejs -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get install libwebp"
+echo -e " ${PURPLE} Usando El Comando: apt-get install libwebp"
 echo -e " ${GREEN}"
 sleep 5
 apt-get install libwebp -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get install ffmpeg"
+echo -e " ${PURPLE} Usando El Comando: apt-get install ffmpeg"
 echo -e " ${GREEN}"
 sleep 5
 apt-get install ffmpeg -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get install wget"
+echo -e " ${PURPLE} Usando El Comando: apt-get install wget"
 echo -e " ${GREEN}"
 sleep 5
 apt-get install wget -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get install imagemagick"
+echo -e " ${PURPLE} Usando El Comando: apt-get install imagemagick"
 echo -e " ${GREEN}"
 sleep 5
 apt-get install imagemagick -y
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: apt-get install tesseract"
+echo -e " ${PURPLE} Usando El Comando: apt-get install tesseract"
 echo -e " ${GREEN}"
 sleep 5
 apt-get install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: npm i imgbb-uploader"
+echo -e " ${PURPLE} Usando El Comando: npm i imgbb-uploader"
 echo -e " ${GREEN}"
 sleep 5
 npm i imgbb-uploader
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Limpando cache npm..."
+echo -e " ${PURPLE} Limpando el cache npm..."
 echo -e " ${GREEN}"
 sleep 5
 npm cache clean -f
@@ -107,13 +107,13 @@ npm i got
 npm install
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Usando o comando: npm audit fix --force"
+echo -e " ${PURPLE} Usando El Comando: npm audit fix --force"
 echo -e " ${GREEN}"
 sleep 5
 npm audit fix --force
 
-echo -e " ${YELLOW} Todas as dependências foram instaladas."
-echo -e " ${YELLOW} Atualização: mudanças mínimas"
-echo -e " ${YELLOW} Traduzido e modifcado por Italu. Base original por MhankBarBar"
-echo -e " ${YELLOW} Use o comando: ${GREEN}npm start ${YELLOW}para iniciar o bot."
+echo -e " ${YELLOW} Se han instalado todas las dependencias."
+echo -e " ${YELLOW} Actualización: cambios mínimos"
+echo -e " ${YELLOW} Creado por Felixcrack. Base original por MhankBarBar"
+echo -e " ${YELLOW} Use el comando: ${GREEN}npm start ${YELLOW}para iniciar o bot."
 
